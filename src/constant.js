2477b8e21c6e4f5f8bdc2df3a3d159261a5323c1
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-08-10 12:22:08 +0700

    improve styles both on desktop & mobile

---
+++ src/constant.js
@@ -0,0 +1,6 @@
+export const LINKEDIN = 'https://www.linkedin.com/in/ridoan-saleh-n-32897385/';
+export const MEDIUM = 'https://medium.com/@ridoansalehnst';
+export const TWITTER = 'https://twitter.com/Rido67720624';
+export const GITHUB = 'https://github.com/ridoansaleh';
+export const STACK_OVERFLOW = 'https://stackoverflow.com/users/4501402/ridoansaleh?tab=profile';
+export const TELKOM_UNIVERSITY = 'https://telkomuniversity.ac.id/id/';
